d05785218896b502a9cf828c375d0c8ed07e3a7e
Brigitte Hofmeister <bhofmei@gmail.com>
2016-07-21 20:10:51 -0400

    general parameters are encoded for url; need to fix decode and implement decode changes

--- js/Util.js
+++ js/Util.js
@@ -1,6 +1,6 @@
 define( "ScreenShotPlugin/Util", [
     'dojo/_base/declare',
-    'dojo/array'
+    'dojo/_base/array'
     ],
 function (
     declare,
@@ -10,34 +10,70 @@ var Util;
 Util = {
     encode: function(inputs){
     // returns string with encode options for screenshot
+        var gInputs = inputs.general;
+        return this._encodeGeneralSettings(gInputs);
     },
 
     decode: function(inStr){
     // returns javascript object to be applied
-
+        return this._decodeGeneralSettings(inStr);
     },
 
-    _encodeGeneralSettings: function(zoom, trackSpacing, showCG, showCHG, showCHH){
+    _encodeGeneralSettings: function(params){
+        // locOver, menu, methylation, nav, trackList, trackSpacing, labels, zoom
         var output = '';
-        if zoom !== undefined:
-            output += 'z' + zoom;
-        if trackSpacing !== undefined:
-            output += 't' + trackSpacing
-        // showCG, showCHG, show CHH must be defined
-        if (! showCG ) || (! showCHG ) || (! showCHH ){
-            output += 'm' + (showCG ? '1' : '0' )+ (showCHG ? '1' : '0' ) + (showCHH ? '1' : '0' )
+        var eLabels = { zoom:'z', trackSpacing:'p', locOver: 'o', trackList:'r', nav:'n', menu:'u', labels:'b', methylation:'m'};
+        var param;
+        for(param in params){
+            var data = params[param];
+            if(param==='methylation')
+                output += eLabels[param] + this._encodeBoolean(data.CG) + this._encodeBoolean(data.CHG) + this._encodeBoolean(data.CHH);
+            else if((param==='zoom')||(param==='trackSpacing'))
+                output += eLabels[param] + data.value;
+            else
+                output += eLabels[param] + this._encodeBoolean(data.value);
         }
         return output;
     },
 
-    _decodeGeneralSettings: function (input){
-        // regex search for zoom
-        outProp = {};
-        var zoomResult = /z([0-9]+)/.exec(input);
-        if (zoomResult !== null){
-            outProp['highResolutionMode'] = zoomResult[1];
-        }
+    _encodeBoolean: function(input){
+        return (input ? '1' : '0');
+    },
 
+    _decodeBoolen: function(input){
+        return(input==='1' ? true : false)
+    },
+
+    _decodeGeneralSettings: function (input){
+        // locOver, menu, methylation, nav, trackList, trackSpacing, labels, zoom
+        var fLabels = { 'z':'highResolutionMode', 'p':'trackPadding', 'o':'show_overview', 'r':'show_tracklist', 'n':'show_nav', 'u':'show_menu', 'b':'showLabels', 'm':'methylation'};
+        var outProp = {basic: {}, view: {}, methylation:{}};
+        var prop;
+        for(prop in fLabels){
+            var p = fLabels[prop];
+            var regex = new RegExp("/"+prop+"([0-9]+)/",'gi');
+            var result = regex.exec(input);
+            if(result !== null){
+                // handle the types
+                if(prop === 'z')
+                    outProp.basic[p] = parseInt(result[1]);
+                else if(prop === 'p'){
+                    outProp.view[p] = parseInt(result[1]);
+                }
+                else if(prop === 'm'){
+                    outProp.methylation.CG =
+                        this._decodeBoolen(result[1].substring(0,1));
+                    outProp.methylation.CHG =
+                        this._decodeBoolen(result[1].substring(1,2));
+                    outProp.methylation.CHH =
+                        this._decodeBoolen(result[1].substring(2,3));
+                }
+                else{
+                    outProp.basic[p] = this._decodeBoolen(result[1]);
+                }
+            }
+        }//end for prop
+        return outProp;
     }
 
 }
--- js/View/Dialog/ScreenShotDialog.js
+++ js/View/Dialog/ScreenShotDialog.js
@@ -1,24 +1,30 @@
 define( "ScreenShotPlugin/View/Dialog/ScreenShotDialog", [
     'dojo/_base/declare',
     'dojo/dom-construct',
+    'dojo/_base/array',
     'dijit/focus',
     'dijit/form/CheckBox',
     'dijit/form/NumberSpinner',
+    'dijit/form/RadioButton',
     'JBrowse/View/Dialog/WithActionBar',
     'dojo/on',
     'dijit/form/Button',
-    'JBrowse/Model/Location'
+    'JBrowse/Model/Location',
+    'ScreenShotPlugin/Util'
     ],
 function (
     declare,
     dom,
+    array,
     focus,
     dijitCheckBox,
     dijitNumberSpinner,
+    dijitRadioButton,
     ActionBarDialog,
     on,
     Button,
-    Location
+    Location,
+    Util
 ) {
 
 return declare (ActionBarDialog,{
@@ -42,8 +48,15 @@ return declare (ActionBarDialog,{
         var ok_button = new Button({
             label: "Render",
             onClick: dojo.hitch(this, function() {
-                console.log(this.parameters);
-                //console.log(this.browser.makeCurrentViewURL());
+                // separate encoded parameters and output parameters
+                var out = this.parameters.output;
+                console.log(out);
+                var gEncode = this.parameters.view;
+                gEncode['methylation']=this.parameters.methylation;
+                gEncode['zoom'] = this.parameters.output.zoom
+                var toEncode = {general: gEncode};
+                console.log(Util.encode(toEncode));
+                console.log(Util.decode(toEncode));
                 //console.log(this.browser.makeCurrentViewURL({nav:0}));
                 this.setCallback && this.setCallback( );
                 //this.hide();
@@ -64,44 +77,38 @@ return declare (ActionBarDialog,{
         // get inital parameters
 
         var mainPaneLeft = dom.create('div',{'id':'screenshot-dialog-pane-left'});
-        dom.create('h2',{'innerHTML':'General configurations'}, mainPaneLeft);
+        dom.create('h2',{'innerHTML':'General configuration options'}, mainPaneLeft);
         var table = dom.create('table',{'id':'screenshot-dialog-opt-table'},mainPaneLeft);
         // check box parameters -> location overview, tracklist, nav, menu bars
-        var boxParam = thisB.parameters.box;
-        var param;
-        for(param in boxParam){
-            var data = boxParam[param];
-            var row = dom.create('tr',{'id':'screenshot-dialog-row-'+param},table);
-            dom.create('td',{'innerHTML':data.title,'class':'screenshot-dialog-pane-label'}, row);
-            var boxD = dom.create('td',{},row);
-            var box = new dijitCheckBox({
-               id:'screenshot-dialog-opt-box-'+param,
-                _prop: param,
-                checked: data.value
-            });
-            box.onClick = dojo.hitch(thisB, '_setParameter', box);
-            box.placeAt(boxD,'first');
-        } // end for param
 
-        // spinner parameters -> zoom and track spacing
-        var spinnerParam = thisB.parameters.spinner;
-        for(param in spinnerParam){
-            var data = spinnerParam[param];
+        var viewParam = thisB.parameters.view;
+        var param;
+        for(param in viewParam){
+            var data = viewParam[param];
             var row = dom.create('tr',{'id':'screenshot-dialog-row-'+param},table);
             dom.create('td',{'innerHTML':data.title,'class':'screenshot-dialog-pane-label'}, row);
-            var spinD = dom.create('td',{},row);
-            var spinner = new dijitNumberSpinner({
-                id:'screenshot-dialog-'+param+'-spinner',
-                value: data.value,
-                _prop:param,
-                constraints: (param === 'zoom'? {min:1,max:10} : {min:0,max:40}),
-                smallDelta:(param === 'zoom' ? 1 : 5),
-                intermediateChanges:true,
-                style:"width:50px;"
-            });
-            spinner.onChange = dojo.hitch(thisB, '_setParameter',spinner);
-            spinner.placeAt(spinD,'first');
-        }
+            var td = dom.create('td',{'class':'screenshot-dialog-pane-input'},row);
+            var input;
+            if(param === 'trackSpacing'){
+                input = new dijitNumberSpinner({
+                    id:'screenshot-dialog-'+param+'-spinner',
+                    value: data.value,
+                    _prop:param,
+                    constraints: {min:0,max:40},
+                    smallDelta:5,
+                    intermediateChanges:true,
+                    style:"width:50px;"
+                });
+            }else{
+                input = new dijitCheckBox({
+                    id:'screenshot-dialog-opt-box-'+param,
+                    _prop: param,
+                    checked: data.value
+                });
+            }
+            input.onClick = dojo.hitch(thisB, '_setParameter', input);
+            input.placeAt(td,'first');
+        } // end for param
 
         // methylation -> if plugin is installed
         if(thisB.browser.plugins.hasOwnProperty('MethylationPlugin')){
@@ -123,8 +130,56 @@ return declare (ActionBarDialog,{
             }
         }
 
+        // Pane bottom is for output
+        var mainPaneBottom = dom.create('div',{'id':'screenshot-dialog-pane-right'});
+        dom.create('h2',{'innerHTML':'Output configuration options'}, mainPaneBottom);
+        var tableB = dom.create('table',{'id':'screenshot-dialog-opt-table'},mainPaneBottom);
+
+        // output options -> format (PNG, JPEG, PDF), height, width
+        var outParam = thisB.parameters.output;
+        for(param in outParam){
+            var data = outParam[param];
+            if(param === 'format'){
+                var row = dom.create('tr',{'id':'screenshot-dialog-row-'+param,'colspan':2},tableB);
+                dom.create('td',{'innerHTML':data.title,'class':'screenshot-dialog-pane-label'}, row);
+                var row2 = dom.create('tr',{'class':'screenshot-dialog-pane-input'},tableB);
+                var outD = dom.create('td',{'colspan':2},row2);
+                // 3 check boxes
+                var formatTypes = ['PNG','JPG','PDF'];
+                array.forEach(formatTypes, function(f){
+                    var btn = new dijitRadioButton({
+                        id: 'screenshot-dialog-output-'+f,
+                        checked: f === thisB.parameters.output.format.value,
+                        value: f,
+                        _prop: param
+                    });
+                    btn.onClick = dojo.hitch(thisB, '_setParameter', btn);
+                    dom.create('span',{innerHTML:f},outD);
+                    outD.appendChild(btn.domNode);
+                });
+            } else {
+                // number spinners
+                var data = outParam[param];
+                var row = dom.create('tr',{'id':'screenshot-dialog-row-'+param},tableB);
+                dom.create('td',{'innerHTML':data.title,'class':'screenshot-dialog-pane-label'}, row);
+                var spinD = dom.create('td',{'class':'screenshot-dialog-pane-input'},row);
+                var spinner = new dijitNumberSpinner({
+                    id:'screenshot-dialog-'+param+'-spinner',
+                    value: data.value,
+                    _prop:param,
+                    constraints: (param === 'zoom' ? {min:1,max:10} : {min:100,max:5000,pattern:'###0'}),
+                    smallDelta:(param === 'zoom' ? 1 : 100),
+                    intermediateChanges:true,
+                    style:"width:75px;"
+                });
+                spinner.onChange = dojo.hitch(thisB, '_setParameter',spinner);
+                spinner.placeAt(spinD,'first');
+            }
+        }
+
         this.set('content', [
-            mainPaneLeft
+            mainPaneLeft,
+            mainPaneBottom
         ] );
 
         this.inherited( arguments );
@@ -134,22 +189,31 @@ return declare (ActionBarDialog,{
         this.inherited(arguments);
         window.setTimeout( dojo.hitch( this, 'destroyRecursive' ), 500 );
     },
+
     _setMethylation: function(box){
         if(this.parameters.methylation.hasOwnProperty(box._prop)){
             this.parameters.methylation[box._prop] = box.checked;
         }
     },
+
     _setParameter: function(input){
         var prop = input._prop;
+        // format radio box parameter
+        if(prop === 'format'){
+            if(input.checked && this.parameters.output.hasOwnProperty(prop))
+                this.parameters.output[prop].value = input.value;
+        }
         // check box parameters
-        if(input.hasOwnProperty('checked')){
-            if(this.parameters.box.hasOwnProperty(prop))
-                this.parameters.box[prop].value = !! input.checked;
+        else if(input.hasOwnProperty('checked')){
+            if(this.parameters.view.hasOwnProperty(prop))
+                this.parameters.view[prop].value = !! input.checked;
         }
         // else spinner
         else{
-            if(this.parameters.spinner.hasOwnProperty(prop))
-                this.parameters.spinner[prop].value = input.value;
+            if(this.parameters.view.hasOwnProperty(prop))
+                this.parameters.view[prop].value = input.value;
+            else if(this.parameters.output.hasOwnProperty(prop))
+                this.parameters.output[prop].value = input.value;
         }
     },
 
@@ -168,8 +232,12 @@ return declare (ActionBarDialog,{
         var nav = { value: config.show_nav, title:'Show navigation bar' };
         var menu = { value: config.show_menu, title:'Show menu bar' }
         var labels = {value:true, title:'Show track labels'}
-       return { spinner: {zoom: zoom, trackSpacing: trackSpacing},
-               box:{locOver, trackList, nav, menu, labels}, methylation:{CG:true, CHG:true, CHH:true} }
+        // output parameters
+        var format = {value: 'PNG', title: 'Output format'}
+        var width = {value: 3300, title: 'Width (px)'}
+        var height = {value: 2400, title: 'Height (px)'}
+
+       return { view:{trackSpacing, locOver, trackList, nav, menu, labels}, methylation:{CG:true, CHG:true, CHH:true}, output: {format, zoom, width, height} }
     }
 });
 });
