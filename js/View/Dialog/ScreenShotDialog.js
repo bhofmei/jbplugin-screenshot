56a34328ae765c1db11934ee7aea071d940aa36b
Brigitte Hofmeister <bhofmei@gmail.com>
2018-04-06 09:54:35 -0400

    some clean up

--- js/View/Dialog/ScreenShotDialog.js
+++ js/View/Dialog/ScreenShotDialog.js
@@ -61,9 +61,9 @@ define("ScreenShotPlugin/View/Dialog/ScreenShotDialog", [
         this.setCallback = args.setCallback || function () {};
         this.cancelCallback = args.cancelCallback || function () {};
         this.vTracks = this.browser.view.visibleTracks();
-        console.log(this.vTracks);
+        //console.log(this.vTracks);
         this.pluginConfig = args.config || {};
-        console.log(this.pluginConfig);
+        //console.log(this.pluginConfig);
         this.trackParameters = this._getTrackParameters();
       },
 
